edb40edfb89a81e99fb37cdef210330e9165f1c9
rick-qiu <qiudejun@gmail.com>
2013-10-23 07:09:20 +0000

    memory management

---
+++ mem.hxx
@@ -0,0 +1,12 @@
+#ifndef CXX_MEM_HXX
+#define CXX_MEM_HXX
+
+namespace cxx {
+    namespace mem {
+        namespace smart_pointer {
+        }
+        namespace allocator {
+        }
+    }
+}
+#endif
