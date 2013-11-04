56b3266486cbc18ff7b0fa05023a558bd74da2b6
rick-qiu <qiudejun@gmail.com>
2013-11-04 04:47:37 +0000

    more stuff

--- thread.cxx
+++ thread.cxx
@@ -5,10 +5,12 @@
 #include <iostream>
 #include <chrono>
 
+// $CXX -std=c++11 -lpthread -g thread.cxx -o test
 int main(int argc, char *argv[]) {
     using namespace std;
     printf("the number of hard cores: %d\n", thread::hardware_concurrency());
     chrono::milliseconds dura(2000);
-    thread t([](){ printf("wait for a duration\n");});
+    thread t([](const chrono::milliseconds& dura){ while(true) {printf("wait for a duration\n"); this_thread::sleep_for(dura);}}, dura);
+    t.join();
     return 0;
 }
