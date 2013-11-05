af4b850fafab8eaf2d80298bfa1b80f3449d54ce
rick-qiu <qiudejun@gmail.com>
2013-11-05 04:24:59 +0000

    test ref

---
+++ ref.cxx
@@ -0,0 +1,17 @@
+
+#include <cstdio>
+
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+class TestClass {
+    int count;
+    int id;
+};
+
+int main(int argc, char *argv[]) {
+    vector<reference_wrapper<TestClass>> vi{};
+    return 0;
+}
